f84ae3c527c82fd99a33bf7cf52459665be54a69
Richard Zhu <richardzhu64@gmail.com>
2020-08-25 18:30:20 -0500

    image fix

--- app/data/projects.js
+++ app/data/projects.js
@@ -5,7 +5,7 @@ const data = [
     subtitle: 'GOV 1005 Final Project',
     link: 'https://richard-zhu-64.shinyapps.io/Oscars_Local/',
     source: 'https://github.com/richardzhu64/oscars-so-local',
-    image: '/images/projects/oscars_so_local.jpg',
+    image: '/images/projects/oscars_so_local.JPG',
     date: '2020-04-20',
     desc: 'Analysis of the Oscars and other film awards by geography and demographics over time.' +
           'Data scraped from AMPAS, BAFTA, Cannes Wikipedia Tables and Kaggle open datasets. Built using R and Shiny.'
@@ -13,7 +13,7 @@ const data = [
   }, {
     title: 'HODP Student Wiki',
     link: 'https://wiki.hodp.org',
-    image: '/images/projects/hodp_wiki.jpg',
+    image: '/images/projects/hodp_wiki.JPG',
     date: '2020-02-01',
     desc: 'Harvard Open Data Project intiative for student-driven wiki with information about Harvard classes, \
            student organizations, housing, and student life.',
@@ -22,7 +22,7 @@ const data = [
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: 'https://medium.com/harvard-open-data-project/mapping-the-hometowns-of-harvards-student-athletes-daf82fea501f',
     source: "https://github.com/richardzhu64/HODPAthletes",
-    image: '/images/projects/harvard_athletes.jpg',
+    image: '/images/projects/harvard_athletes.JPG',
     date: '2019-12-20',
     desc: 'Data journalism article investigating Harvard student-athlete hometowns by sport, geography, and median income.'
   }, {
@@ -30,7 +30,7 @@ const data = [
     subtitle: 'A website to connect debaters with coaches, judges, and practice partners',
     link: 'https://www.youtube.com/watch?v=ljO_3Y3H7FM',
     source: 'https://github.com/richardzhu64/debate-harmony',
-    image: '/images/projects/debate_harmony.jpg',
+    image: '/images/projects/debate_harmony.JPG',
     date: '2018-12-06',
     desc: 'Web app designed to help debaters find compatible evidence trades, judging, coaching, and practice debate partners. Built using Python, SQLite, Flask.',
   },
